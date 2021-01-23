10931cee000662c8e521822d2762fa2fc8fb93e1
Gaxiya <deprey924@gmail.com>
2021-01-23 15:40:02 +0300

    update first.js

---
+++ first.js
@@ -0,0 +1,23 @@
+function temperature()
+{
+    var cel= prompt("type temprature in Celsius "), fahr =0;
+    fahr =cel*1.8 +32;
+    alert ("Temperature in Fahrenheit = " + fahr);
+
+}
+temperature();
+
+function first2(){
+    var admin=" ";
+    alert("Admin = "+ admin) ;
+    var name="Василий ";
+    alert("Name = "+ name);
+    admin= name;
+    alert("Admin = "+ admin);
+
+}
+first2();
+first3();
+function first3(){
+    alert(1000+ "108");
+}
